919d2b3e265ae8833495d43e42f0e988ba009142
Cole Alexander <cbielefeld@cnm.edu>
2020-04-28 08:59:43 -0600

    PWP Milestone 1. Made changes to the HTML formatting and tweaked a few things.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PWP Milestone 1</title>
+    <text>Purpose, audience & goal:
+    </text>
+
+    <p>1. Purpose - I'd like this PWP to help me acomplish a preface of who I am, what I do and where I'm
+        headed. I want this PWP to primarily be geared towards making me look desirable for tech company recruiters
+        & showing off a bit of my  own skills. </p>
+
+       <p>2. Audience: This website will be geared towards a professional audience seeking professional services
+           and recruiters looking to hire.</p>
+
+        <p>3. Goal: To acquire full-time or part-time employment, commission based work or internship positions. </p>
+</head>
+<body>
+<p>
+    Persona:
+
+<img alt src="confident-Latina-woman.jpg">
+
+    <p>Hazel Garcia
+    Age 32, Recruiter for a startup tech security company in the state of New Mexico.</p>
+
+    <p>Technology: uses predominantly Mac and apple products in the office, and a windows Lenovo Ideapad at home.</p>
+
+    <p>Behaviors and attitudes: Stressed due to family complications, but moves quickly from site to site, spending
+    an average time of 45 seconds. Loves seeing a clean, organized and aesthetically
+    pleasing profiles, and will typically spend more time on these than others.</p>
+
+    <p>Use of the site: Checking to see if I would be a good candidate.</p>
+
+    <p>Goal(s) regarding the use of the site: Seeing enough relevant information to determine that I would be right
+    for the job or an interview, and is provided with ways of getting in touch with me.</p>
+</body>
+</html>
+
