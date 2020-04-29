4f71bce29470e9c75c3ee8aaacac41a9fb583b3c
Cole Alexander <cbielefeld@cnm.edu>
2020-04-28 18:58:43 -0600

    PWP Milestone 1. Made changes to the HTML formatting and tweaked a few things.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,30 +9,39 @@
         headed. I want this PWP to primarily be geared towards making me look desirable for tech company recruiters
         & showing off a bit of my  own skills. </p>
 
-       <p>2. Audience: This website will be geared towards a professional audience seeking professional services
-           and recruiters looking to hire.</p>
+    <p>2. Audience: This website will be geared towards a professional audience seeking professional services
+        and recruiters looking to hire.</p>
 
-        <p>3. Goal: To acquire full-time or part-time employment, commission based work or internship positions. </p>
+    <p>3. Goal: To acquire full-time or part-time employment, commission based work or internship positions. </p>
 </head>
 <body>
 <p>
-    Persona:
+    4. Target Primary Audience Persona:
 
 <img alt src="confident-Latina-woman.jpg">
 
     <p>Hazel Garcia
-    Age 32, Recruiter for a startup tech security company in the state of New Mexico.</p>
+       Age 32, Recruiter for a startup tech security company in the state of New Mexico, works from home. As a
+       millenial, she has grown up with quite a bit of tech, and has reasonably high expectations.
 
-    <p>Technology: uses predominantly Mac and apple products in the office, and a windows Lenovo Ideapad at home.</p>
+    .</p>
 
-    <p>Behaviors and attitudes: Stressed due to family complications, but moves quickly from site to site, spending
-    an average time of 45 seconds. Loves seeing a clean, organized and aesthetically
-    pleasing profiles, and will typically spend more time on these than others.</p>
+    <p>•Technology - uses predominantly Mac and apple products in the office, and a windows Lenovo Ideapad at home.</p>
 
-    <p>Use of the site: Checking to see if I would be a good candidate.</p>
+    <p>•Behaviors and attitudes - Stressed due to family complications, therefore moves quickly from site to site,
+        spending an average time of 30 seconds. Loves seeing a clean, organized and aesthetically
+        pleasing profiles, and will typically spend more time on these than others.</p>
 
-    <p>Goal(s) regarding the use of the site: Seeing enough relevant information to determine that I would be right
-    for the job or an interview, and is provided with ways of getting in touch with me.</p>
+    <p>•Use of the site - Checking to see if I would be a good candidate for hire or interview.</p>
+
+    <p>5. User Story: Hazel is assigned with the duty of getting in touch with new and skilled tech professionals
+        and potential candidates for her company throughout the Albuquerque area. She know's of the various tech
+        bootcamps throughout the city thanks to the workforce and decides to start there to see if there's any new
+        talent. Ms. Garcia comes across my site, sees a quick and concise preface of character, who I am and my
+        experience. She decides to contact me though one of my email shortcuts near the bottom of the page.</p>
+
+    <p>6. Goal(s) regarding the use of the site: Seeing enough relevant information to determine that I would be right
+        for the job or an interview, and is provided with ways of getting in touch with me.</p>
 </body>
 </html>
 
