8f17427c6bc08d62b63de39da30e7d8a5c36aabf
Cole Alexander <cbielefeld@cnm.edu>
2020-05-06 21:32:07 -0600

    PWP Milestone 2. Created the content strategy & wireframe PDF.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Content Strategy & Wireframes</title>
+</head>
+<body>
+
+<h4>Welcome:</h4>
+<p>Near the top of the page, displaying a one to two sentence introduction, e.g., name, location and what I do.</p>
+
+<H4>About me:</H4>
+<p>A bit more personalization and overview into who I am, primarily detailing my strengths and personality. To the
+right hand side of the screen will be table listing core skills & coding knowlege</p>
+
+
+<h4>Portfolio:</h4>
+<P>displaying relevant projects and creations in clickable photo style links</P>
+<h4>Link to Resume</h4>
+<p>Link to Resume, work history / job expertice</p>
+
+
+<h4>Passions:</h4>
+<p>One to two sentances describing my love for building custom computers, trouble shooting them
+    & designing game rooms</p>
+
+<h4>Contact me:</h4>
+<p>message panel, name & email rectangles for someone to place in their info, in-laced with a send button.</p>
+
+
+<h4>Social Media links</h4>
+<p>Near the bottom of the page I'll place clickable social media & business platform connection icons splayed over a dark
+background. Brief name of the platform below the icons, & a little "social medias" header before the border. This will
+include Facebook, GitHub, Linked-In, maybe Twitter & Whatsapp as well.</p>
+
+<h4>Wireframe desktop & mobile concept:</h4>
+<p>https://whimsical.com/FtjgyyX5PW5bKTcDUs2J9M</p>
+
+<img src="PWP-Content%20Wire%20frames.pdf">
+
+</body>
+</html>
